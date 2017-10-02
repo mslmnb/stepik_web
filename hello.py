dc7f2c71c1fbae0130d10479416f97b8985253b9
“mslmnb” <mslmn@rambler.ru>
2017-10-02 21:44:40 +0600

    Begin do WSGI-application

---
+++ hello.py
@@ -0,0 +1,11 @@
+def app(env, start_response):
+    data = environ['QUERY_STRING']
+    d = '/n'.join(str(data).split("&"))
+
+    status = '200 OK'
+    response_headers = [
+        ('Content-type', 'text/plain'),
+        ('Content-Length', str(len(data)))
+    ]
+    start_response(status, response_headers)
+    return [d]
