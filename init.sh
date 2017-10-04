653977cfdc1cf355094bfaf8ebcdb0bb7be4038a
mslmnb <mslmn@rambler.ru>
2017-10-04 16:44:54 +0000

    Create django application

--- init.sh
+++ init.sh
@@ -3,5 +3,6 @@ sudo rm -rf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 
 #gunicorn config
-sudo ln -s /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
+sudo ln -sf /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
+sudo ln -sf /home/box/web/etc/gunicorn_ask.conf /etc/gunicorn.d/ask
 sudo /etc/init.d/gunicorn restart
