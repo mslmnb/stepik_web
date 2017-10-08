335609ef9692929a6bd13be6d674df9a78e9c290
“mslmnb” <mslmn@rambler.ru>
2017-10-08 11:38:18 +0600

    End dO easy Guricon&Django app

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -14,14 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import url, include
-from django.contrib import admin
+#from django.contrib import admin
+#admin.autodiscover()
+#from ask.gala import test
 
 urlpatterns = [
-   url(r'^$', include(qa.urls)),
-   url(r'^login/$', include(qa.urls)),
-   url(r'^signup/$', include(qa.urls)),                                                                                        
-   url(r'^question/\d+/$', include(qa.urls)),                                                                               
-   url(r'^ask/$', include(qa.urls)),                                                                                              
-   url(r'^popular/$', include(qa.urls)),                                                                                      
-   url(r'^new/$', include(qa.urls)),       
+#    url(r"^", test, name='test'),
+     url(r'^', include('qa.urls')),
 ]
--- init.sh
+++ init.sh
@@ -3,6 +3,6 @@ sudo rm -rf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 
 #gunicorn config
-sudo ln -sf /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
-sudo ln -sf /home/box/web/etc/gunicorn_ask.conf /etc/gunicorn.d/ask
-sudo /etc/init.d/gunicorn restart
+#sudo ln -sf /home/box/web/etc/gunicorn_hello.conf /etc/gunicorn.d/test
+#sudo ln -sf /home/box/web/etc/gunicorn_ask.conf /etc/gunicorn.d/ask
+#sudo /etc/init.d/gunicorn restart
