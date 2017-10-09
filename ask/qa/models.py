d9c807d08c99cd76617f1d8883d89c353a3f0dd9
mslmnb <mslmn@rambler.ru>
2017-10-09 16:04:47 +0000

    Begin models for MySql

---
+++ ask/qa/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Question(models.Model):
+    title = models.CharField(max_length=255)
+    text = models.TextField()
+    added_at = models.DateTimeField(auto_now_add=True)
+    rating = models.IntegerField(default=0)
+    author = models.ForeignKey(User)
+    likes = models.TextField()
+
+
+class Answer(models.Model):
+    text = models.TextField()
+    added_at = models.DateTimeField(auto_now_add=True)
+    question = models.ForeignKey(Question)
+    author = models.ForeignKey(User)
+
+class QuestionManager(models.Manager):                              
+    def new(self):
+    return self.order_by('-added_at')
+    def popular(self):
+    return self.order_by('-rating')
+
