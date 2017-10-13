9b479f131487ac80e4ca361a3e0c05e186a2649e
“mslmnb” <mslmn@rambler.ru>
2017-10-13 17:35:18 +0000

    it works

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,24 +1,23 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-class Question(models.Model):
-    title = models.CharField(max_length=255)
-    text = models.TextField()
-    added_at = models.DateTimeField(blank=True, auto_now_add=True)
-    rating = models.IntegerField(default=0)
-    author = models.ForeignKey(User,related_name="question_author")
-    likes = models.ManyToManyField(User,related_name="question_like",blank=True)
-
-
-class Answer(models.Model):
-    text = models.TextField()
-    added_at = models.DateTimeField(auto_now_add=True)
-    question = models.ForeignKey(Question)
-    author = models.ForeignKey(User)
-
 class QuestionManager(models.Manager):                              
     def new(self):
         return self.order_by('-added_at')
     def popular(self):
         return self.order_by('-rating')
 
+class Question(models.Model):                                                   
+    objects = QuestionManager()                                                 
+    title = models.CharField(max_length=255)                                    
+    text = models.TextField()                                                   
+    added_at = models.DateTimeField(blank=True, auto_now_add=True)              
+    rating = models.IntegerField(default=0)                                     
+    author = models.ForeignKey(User,related_name="question_author")             
+    likes = models.ManyToManyField(User,related_name="question_like",blank=True)
+                                        
+class Answer(models.Model):                                                     
+    text = models.TextField()                                                   
+    added_at = models.DateTimeField(auto_now_add=True)                          
+    question = models.ForeignKey(Question)                                      
+    author = models.ForeignKey(User)                                  
